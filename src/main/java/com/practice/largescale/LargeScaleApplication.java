d5b718be98f6be8d0553aad33a5211557cb7f28c
jungwon <mac2001035@gmail.com>
2024-08-26 01:12:14 +0900

    INIT: 대용량 프로젝트 시작.

---
+++ src/main/java/com/practice/largescale/LargeScaleApplication.java
@@ -0,0 +1,13 @@
+package com.practice.largescale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeScaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeScaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/largescale/LargeScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.largescale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeScaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
